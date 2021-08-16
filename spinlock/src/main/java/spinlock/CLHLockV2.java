90acfcb8e50af29f72c591a4bbae4763901bd798
xiongyuxuan <xiongyuxuan@youzan.com>
2021-08-16 10:35:50 +0800

    CLHV2锁支持超时

--- spinlock/src/main/java/spinlock/CLHLockV2.java
+++ spinlock/src/main/java/spinlock/CLHLockV2.java
@@ -56,6 +56,40 @@ public class CLHLockV2 implements SpinLock{
         }
     }
 
+    /**
+     * 加锁（timeout毫秒内未成功加锁，则主动退出加锁）
+     * */
+    public boolean lock(long timeout) {
+        CLHNode currentNode = curNode.get();
+        currentNode.isLocked = true;
+
+        // cas的设置为当前tail为新的tail节点
+        currentNode.prev = tail.getAndSet(currentNode);
+
+        final long deadline = System.currentTimeMillis() + timeout;
+
+        while(true){
+            while(currentNode.prev.isLocked){
+                timeout = deadline - System.currentTimeMillis();
+                if (timeout <= 0L) {
+                    // 加锁超时，退出加锁
+                    unlock();
+                    // 当前已经超时，加锁失败返回false
+                    return false;
+                }
+            }
+
+            // 内层while循环结束，说明前驱节点已经释放了锁
+            CLHNode prevNode = currentNode.prev;
+            if(prevNode == head){
+                // 如果前驱节点为head（Dummy节点）
+                return true;
+            }else{
+                currentNode.prev = prevNode.prev;
+            }
+        }
+    }
+
     @Override
     public void unlock() {
         CLHNode currentNode = curNode.get();
--- spinlock/src/test/java/CLHLockV2Test.java
+++ spinlock/src/test/java/CLHLockV2Test.java
@@ -1,8 +1,6 @@
-import spinlock.CLHLockV1;
 import spinlock.CLHLockV2;
-import spinlock.SpinLock;
-import util.SpinLockTestUtil;
 
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -13,16 +11,55 @@ import java.util.concurrent.Executors;
 public class CLHLockV2Test {
 
     public static void main(String[] args) throws InterruptedException {
-        SpinLock spinLock = new CLHLockV2();
+        CLHLockV2 CLHLockV2 = new CLHLockV2();
         int sumCount = 10;
         int repeatSum = 1000;
         ExecutorService executorService = Executors.newFixedThreadPool(sumCount);
-        int result = SpinLockTestUtil.testConcurrentSum(executorService, spinLock, sumCount, repeatSum);
+        int result = testConcurrentSum(executorService, CLHLockV2, sumCount, repeatSum, 20);
         executorService.shutdown();
 
         System.out.println(result);
         if (sumCount*repeatSum != result) {
             throw new RuntimeException("testCLHLockV2 error: sumCount != result result=" + result);
         }
+    }
+
+    public static int testConcurrentSum(ExecutorService executorService, CLHLockV2 CLHLockV2, int sumCount, int repeatSum, long timeout) throws InterruptedException {
+        CountDownLatch driverLatch = new CountDownLatch(1);
+        CountDownLatch barrierLatch = new CountDownLatch(sumCount);
+
+        final int[] count = {0};
+        for (int i = 0; i < sumCount; i++) {
+            executorService.execute(()->{
+                try {
+                    driverLatch.await();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+
+                for(int j=0;j<repeatSum;j++) {
+                    // 用自旋锁保护自增操作并发时的线程安全
+                    while(true){
+                        if(CLHLockV2.lock(timeout)){
+                            System.out.println("加锁成功");
+                            count[0]++;
+                            CLHLockV2.unlock();
+                            break;
+                        }
+                    }
+                }
+
+                barrierLatch.countDown();
+            });
+        }
+
+        long start = System.currentTimeMillis();
+        driverLatch.countDown();
+
+        barrierLatch.await();
+        long end = System.currentTimeMillis();
+        System.out.println("total cost=" + (end-start) + "ms");
+
+        return count[0];
     }
 }
