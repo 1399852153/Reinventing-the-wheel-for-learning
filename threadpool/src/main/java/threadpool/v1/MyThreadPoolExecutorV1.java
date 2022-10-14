dac17ebcad8b8c63ff3e65ce28e78fb7da8166af
xiongyx <1399852153@qq.com>
2022-10-15 02:19:00 +0800

    自己动手实现线程池v1版本

--- threadpool/src/main/java/threadpool/v1/MyThreadPoolExecutorV1.java
+++ threadpool/src/main/java/threadpool/v1/MyThreadPoolExecutorV1.java
@@ -6,6 +6,7 @@ import threadpool.MyThreadPoolExecutor;
 import java.util.HashSet;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.ReentrantLock;
@@ -66,6 +67,12 @@ public class MyThreadPoolExecutorV1 implements MyThreadPoolExecutor {
      * 跟踪线程池曾经有过的最大线程数量（只能在mainLock的并发保护下更新）
      */
     private int largestPoolSize;
+
+    /**
+     * 是否允许核心线程在idle一定时间后被销毁（和非核心线程一样）
+     * */
+    private volatile boolean allowCoreThreadTimeOut;
+
 
     public MyThreadPoolExecutorV1(int corePoolSize,
                                   int maximumPoolSize,
@@ -145,6 +152,122 @@ public class MyThreadPoolExecutorV1 implements MyThreadPoolExecutor {
     }
 
     public void runWorker(MyWorker myWorker) {
+        // 时worker线程的run方法调用的，此时的current线程的是worker线程
+        Thread workerThread = Thread.currentThread();
+        Runnable task = myWorker.firstTask;
+
+        // todo state设置为0，开启中断
+        // myWorker.unlock();
+
+        // 默认线程是由于中断退出的
+        boolean completedAbruptly = true;
+        try {
+            // worker线程处理主循环，核心逻辑
+            while (task != null || (task = getTask()) != null) {
+                try {
+                    // 任务执行前的钩子函数
+                    beforeExecute(workerThread, task);
+                    Throwable thrown = null;
+                    try {
+                        // 拿到的任务开始执行
+                        task.run();
+                    } catch (RuntimeException | Error x) {
+                        // 使用thrown收集抛出的异常，传递给afterExecute
+                        thrown = x;
+                        // 同时抛出错误，从而中止主循环
+                        throw x;
+                    } catch (Throwable x) {
+                        // 使用thrown收集抛出的异常，传递给afterExecute
+                        thrown = x;
+                        // 同时抛出错误，从而中止主循环
+                        throw new Error(x);
+                    } finally {
+                        // 任务执行后的钩子函数，如果任务执行时抛出了错误/异常，thrown不为null
+                        afterExecute(task, thrown);
+                    }
+                } finally {
+                    // 将task设置为null,令下一次while循环通过getTask获得新任务
+                    task = null;
+                    // 无论执行时是否存在异常，已完成的任务数加1
+                    myWorker.completedTasks++;
+                }
+
+            }
+            // 由于没有可执行的任务了。线程正常的退出
+            completedAbruptly = false;
+        }finally {
+            processWorkerExit(myWorker, completedAbruptly);
+        }
+
+    }
+
+    /**
+     * 尝试着从阻塞队列里获得待执行的任务
+     * @return 返回null代表工作队列为空，没有需要执行的任务; 或者当前worker线程满足了需要退出的一些条件
+     *         返回对应的任务
+     * */
+    private Runnable getTask() {
+        boolean timedOut = false;
+
+        while(true) {
+            // 工作队列为空
+            if (workQueue.isEmpty()) {
+                // 当前工作线程需要退出，先将worker计数器减一
+                decrementWorkerCount();
+                // 返回null，令当前worker线程退出
+                return null;
+            }
+
+            // 获得当前线程个数
+            int workCount = this.workerCount.get();
+
+            // 有两种情况需要指定超时时间的方式从阻塞队列workQueue中获取任务（即timed为true）
+            // 1.线程池配置参数allowCoreThreadTimeOut为true，即允许核心线程在idle一定时间后被销毁
+            //   所以allowCoreThreadTimeOut为true时，需要令timed为true，这样可以让核心线程也在一定时间内获取不到任务(idle状态)而被销毁
+            // 2.线程池配置参数allowCoreThreadTimeOut为false,但当前线程池中的线程数量workCount大于了指定的核心线程数量corePoolSize
+            //   说明当前有一些非核心的线程正在工作，而非核心的线程在idle状态一段时间后需要被销毁
+            //   所以此时也令timed为true，让这些线程在keepAliveTime时间内由于队列为空拉取不到任务而返回null，将其销毁
+            boolean timed = allowCoreThreadTimeOut || workCount > corePoolSize;
+
+            // 有四种情况不需要往下执行，代表
+            // todo 待完善
+            if ((workCount > maximumPoolSize || (timed && timedOut))
+                    && (workCount > 1 || workQueue.isEmpty())) {
+                if (compareAndDecrementWorkerCount(workCount)) {
+                    // 满足上述条件，说明当前线程需要被销毁了，返回null
+                    return null;
+                }
+
+                // compareAndDecrementWorkerCount方法由于并发的原因cas执行失败，continue循环重试
+                continue;
+            }
+
+            try {
+                // 根据上面的逻辑的timed标识，决定以什么方式从阻塞队列中获取任务
+                Runnable r = timed ?
+                        // timed为true，通过poll方法指定获取任务的超时时间（如果指定时间内没有队列依然为空，则返回）
+                        workQueue.poll(keepAliveTime, TimeUnit.NANOSECONDS) :
+                        // timed为false，通过take方法无限期的等待阻塞队列中加入新的任务
+                        workQueue.take();
+                if (r != null) {
+                    // 获得了新的任务，getWork正常返回对应的任务对象
+                    return r;
+                }else{
+                    // 否则说明timed=true，且poll拉取任务时超时了
+                    timedOut = true;
+                }
+            } catch (InterruptedException retry) {
+                // poll or take任务等待时worker线程被中断了，捕获中断异常
+                // timeout = false,标识拉取任务时没有超时
+                timedOut = false;
+            }
+        }
+    }
+
+    /**
+     * 处理worker线程退出
+     * */
+    private void processWorkerExit(MyWorker myWorker, boolean completedAbruptly) {
         // todo
     }
 
@@ -185,8 +308,8 @@ public class MyThreadPoolExecutorV1 implements MyThreadPoolExecutor {
         MyWorker newWorker = null;
         try {
             // 创建一个新的worker
-            newWorker = new MyWorker(this.threadFactory, firstTask);
-            final Thread myWorkerThread = newWorker.getThread();
+            newWorker = new MyWorker(firstTask);
+            final Thread myWorkerThread = newWorker.thread;
             // 线程创建成功
             if (myWorkerThread != null) {
                 final ReentrantLock mainLock = this.mainLock;
@@ -250,6 +373,24 @@ public class MyThreadPoolExecutorV1 implements MyThreadPoolExecutor {
         } while (!compareAndDecrementWorkerCount(this.workerCount.get()));
     }
 
+    public ThreadFactory getThreadFactory() {
+        return threadFactory;
+    }
+
+    public void allowCoreThreadTimeOut(boolean value) {
+        if (value && keepAliveTime <= 0) {
+            throw new IllegalArgumentException("Core threads must have nonzero keep alive times");
+        }
+        // 判断一下新旧值是否相等，避免无意义的volatile变量更新，导致不必要的cpu cache同步
+        if (value != allowCoreThreadTimeOut) {
+            allowCoreThreadTimeOut = value;
+            // todo
+//            if (value) {
+//                interruptIdleWorkers();
+//            }
+        }
+    }
+
     /**
      * 当创建worker出现异常失败时，对之前的操作进行回滚
      * 1 如果新创建的worker加入了workers集合，将其移除
@@ -269,6 +410,43 @@ public class MyThreadPoolExecutorV1 implements MyThreadPoolExecutor {
             // tryTerminate();
         } finally {
             mainLock.unlock();
+        }
+    }
+
+    // ===================== 留给子类做拓展的方法 =======================
+    /**
+     * 任务执行前
+     * */
+    protected void beforeExecute(Thread t, Runnable r) {
+        // 默认为无意义的空方法
+    }
+
+    /**
+     * 任务执行后
+     * */
+    protected void afterExecute(Runnable r, Throwable t) {
+
+    }
+
+
+    // =============================== worker线程内部类 =====================================
+    private final class MyWorker implements Runnable{
+
+        final Thread thread;
+        Runnable firstTask;
+        volatile long completedTasks;
+
+
+        public MyWorker(Runnable firstTask) {
+            this.firstTask = firstTask;
+
+            // newThread可能是null
+            this.thread = getThreadFactory().newThread(this);
+        }
+
+        @Override
+        public void run() {
+            runWorker(this);
         }
     }
 }
