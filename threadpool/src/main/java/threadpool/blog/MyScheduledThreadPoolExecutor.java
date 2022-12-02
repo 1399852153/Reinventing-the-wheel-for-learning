c5a542503472e69feb357b02aca27db694e2bd58
shanreng <1399852153@qq.com>
2022-12-02 21:12:04 +0800

    MyScheduledThreadPoolExecutor 实现

--- threadpool/src/main/java/threadpool/blog/MyScheduledThreadPoolExecutor.java
+++ threadpool/src/main/java/threadpool/blog/MyScheduledThreadPoolExecutor.java
@@ -34,15 +34,57 @@ public class MyScheduledThreadPoolExecutor extends MyThreadPoolExecutorV2 implem
         return System.nanoTime();
     }
 
+    /**
+     * 获得下一次调度的绝对时间
+     * */
     private long triggerTime(long delay, TimeUnit unit) {
+        // 统一转成nanos级别计算
         return triggerTime(unit.toNanos((delay < 0) ? 0 : delay));
     }
 
     /**
-     * Returns the trigger time of a delayed action.
+     * 获得下一次调度的绝对时间
+     * @param delay 延迟时间（单位nanos）
      */
     long triggerTime(long delay) {
-        return now() + ((delay < (Long.MAX_VALUE >> 1)) ? delay : overflowFree(delay));
+        if(delay < (Long.MAX_VALUE >> 1)){
+            // delay小于Long.MAX_VALUE/2，肯定不会发生compareTo时的溢出问题，直接正常累加delay即可
+            return now() + delay;
+        }else{
+            // delay大于Long.MAX_VALUE/2，可能会发生compareTo时的溢出问题，在overflowFree检查并做必要的修正
+            return now() + overflowFree(delay);
+        }
+    }
+
+    /**
+     * Constrains the values of all delays in the queue to be within
+     * Long.MAX_VALUE of each other, to avoid overflow in compareTo.
+     * This may occur if a task is eligible to be dequeued, but has
+     * not yet been, while some other task is added with a delay of
+     * Long.MAX_VALUE.
+     *
+     * 处理delay延迟时间溢出的问题
+     * 举个例子：
+     * 1. 当前队列里有一个理应出队被处理但实际还未处理的任务A（getDelay < 0），此时新提交了一个getDaley=Long.MAX_VALUE的任务B
+     *    理论上来说，B是要等待很久很久才能执行的，入队后应该是放在A后面的，即A先于B执行（AB）
+     * 2. 但如果不进行溢出处理的话，则入队过程中compareTo将会得到意料之外的结果
+     *    假设A的getDelay为-10，B的getDelay为Long.MAX_VALUE，以延迟执行的时间作为依据A.compareTo(B)应该返回-1,代表A优先于B执行
+     *    但实际上-10减去Long.MAX_VALUE会发生溢出，而得到一个正数，compareTo会返回1，导致B先于A执行（BA）
+     * 3. 这样就发生了错乱，由于B的getDelay大的离谱，使得排在后面的A永远无法被执行了。所以必须通过overflowFree来限制一下
+     */
+    private long overflowFree(long delay) {
+        // 获得当前队列中最早需要被调度的任务（peek方式获取）
+        Delayed head = (Delayed) super.getQueue().peek();
+        if (head != null) {
+            long headDelay = head.getDelay(NANOSECONDS);
+            // 当头结点延迟时间headDelay小于0，而参数delay非常大时（比如Long.MAX_VALUE）就会发生溢出（delay - headDelay < 0）
+            // 正常的delay参数值减去一个负数是不可能小于0的
+            if (headDelay < 0 && (delay - headDelay < 0)) {
+                // 发生了溢出，则当前的delay值设置为Long.MAX_VALUE加上一个负值，保证当前的delay值的任务在入队时compareTo不会溢出，会被放在正确的位置上
+                delay = Long.MAX_VALUE + headDelay;
+            }
+        }
+        return delay;
     }
 
     /**
