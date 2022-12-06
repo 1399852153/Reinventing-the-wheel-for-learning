2e259636f50ef149c7857238eb0274972bb9f723
shanreng <1399852153@qq.com>
2022-12-06 21:46:26 +0800

    MyScheduledThreadPoolExecutor 实现

--- threadpool/src/main/java/threadpool/blog/MyScheduledThreadPoolExecutor.java
+++ threadpool/src/main/java/threadpool/blog/MyScheduledThreadPoolExecutor.java
@@ -3,8 +3,13 @@ package threadpool.blog;
 import threadpool.MyRejectedExecutionHandler;
 import threadpool.MyScheduledExecutorService;
 
+import java.util.AbstractQueue;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
 
 import static java.util.concurrent.TimeUnit.NANOSECONDS;
 
@@ -98,6 +103,55 @@ public class MyScheduledThreadPoolExecutor extends MyThreadPoolExecutorV2 implem
     }
 
     /**
+     * 尝试重新提交并执行周期性任务
+     * */
+    void reExecutePeriodic(RunnableScheduledFuture<?> task) {
+        if (canRunInCurrentRunState(true)) {
+            // 当前线程池状态允许执行任务，将任务加入到工作队列中去
+            super.getQueue().add(task);
+            // 再次检查，如果状态发生了变化，不允许了，则通过remove方法将刚加入的任务移除掉，实现回滚
+            // 和ThreadPoolExecutor一致都是为了让shutdown/stop状态的线程池尽量在状态变更和提交新任务出现并发时，不要去执行新任务尽早终止线程池
+            if (!canRunInCurrentRunState(true) && super.remove(task)) {
+                task.cancel(false);
+            } else {
+                // 确保至少有一个工作线程会处理当前提交的任务
+                super.ensurePrestart();
+            }
+        }
+    }
+
+    /**
+     * Returns true if can run a task given current run state
+     * and run-after-shutdown parameters.
+     *
+     * @param periodic true if this task periodic, false if delayed
+     */
+    boolean canRunInCurrentRunState(boolean periodic) {
+        if(periodic){
+            // 周期性任务，由continueExistingPeriodicTasksAfterShutdown决定
+            return super.isRunningOrShutdown(continueExistingPeriodicTasksAfterShutdown);
+        }else{
+            // 非周期性任务（普通延迟任务），由executeExistingDelayedTasksAfterShutdown决定
+            return super.isRunningOrShutdown(executeExistingDelayedTasksAfterShutdown);
+        }
+    }
+
+    @Override
+    public ScheduledFuture<?> schedule(Runnable command, long delay, TimeUnit unit) {
+        return null;
+    }
+
+    @Override
+    public ScheduledFuture<?> scheduleAtFixedRate(Runnable command, long initialDelay, long period, TimeUnit unit) {
+        return null;
+    }
+
+    @Override
+    public ScheduledFuture<?> scheduleWithFixedDelay(Runnable command, long initialDelay, long delay, TimeUnit unit) {
+        return null;
+    }
+
+    /**
      * 调度任务对象
      * */
     private class MyScheduledFutureTask<V> extends FutureTask<V> implements RunnableScheduledFuture<V>{
@@ -127,8 +181,7 @@ public class MyScheduledThreadPoolExecutor extends MyThreadPoolExecutorV2 implem
         RunnableScheduledFuture<V> outerTask = this;
 
         /**
-         * 基于二叉堆的延迟队列中的下标，用于快速的定位
-         * Index into delay queue, to support faster cancellation.
+         * 基于二叉堆的延迟队列中的数组下标，用于快速的查找、定位
          * */
         int heapIndex;
 
@@ -272,51 +325,195 @@ public class MyScheduledThreadPoolExecutor extends MyThreadPoolExecutorV2 implem
     }
 
     /**
-     * 尝试重新提交并执行周期性任务
+     * 为调度任务线程池ScheduledThreadPoolExecutor专门定制的工作队列
+     * 1.基于完全二叉堆结构，令执行时间最小（最近）的任务始终位于堆顶（即队列头） ==> 小顶堆
+     * 2.实现上综合了juc包下的DelayQueue和PriorityQueue的功能，并加上了一些基于ScheduledThreadPoolExecutor的一些逻辑
+     *   建议读者在理解了PriorityQueue、DelayQueue原理之后再来学习其工作机制，循序渐进而事半功倍
      * */
-    void reExecutePeriodic(RunnableScheduledFuture<?> task) {
-        if (canRunInCurrentRunState(true)) {
-            // 当前线程池状态允许执行任务，将任务加入到工作队列中去
-            super.getQueue().add(task);
-            // 再次检查，如果状态发生了变化，不允许了，则通过remove方法将刚加入的任务移除掉，实现回滚
-            // 和ThreadPoolExecutor一致都是为了让shutdown/stop状态的线程池尽量在状态变更和提交新任务出现并发时，不要去执行新任务尽早终止线程池
-            if (!canRunInCurrentRunState(true) && super.remove(task)) {
-                task.cancel(false);
-            } else {
-                // 确保至少有一个工作线程会处理当前提交的任务
-                super.ensurePrestart();
+    static class DelayedWorkQueue extends AbstractQueue<Runnable> implements BlockingQueue<Runnable> {
+        /**
+         * 完全二叉堆底层数组的初始容量
+         * */
+        private static final int INITIAL_CAPACITY = 16;
+
+        /**
+         * 完全二叉堆底层数组
+         * */
+        private RunnableScheduledFuture<?>[] queue = new RunnableScheduledFuture<?>[INITIAL_CAPACITY];
+
+        /**
+         * 互斥锁，用于入队等操作时的并发控制
+         * */
+        private final ReentrantLock lock = new ReentrantLock();
+
+        /**
+         * 队列中任务元素的数量
+         * */
+        private int size = 0;
+
+        /**
+         * 等待执行队列头（最早应该执行的）任务的线程池工作线程
+         * 为什么会引入一个这个呢？是为了减少其它线程take获取新任务时不必要的等待
+         * 因为额外引入了一个操作系统层面的定时器，await带超时时间比无限时间的await性能要差一些
+         * */
+        private Thread leader = null;
+
+        /**
+         * 当一个队列头部的任务可以被执行时，通知等待在available上的工作线程
+         * */
+        private final Condition available = lock.newCondition();
+
+        // ============================= 内部private的辅助方法 ================================
+        private void setIndex(RunnableScheduledFuture<?> f, int index) {
+            if (f instanceof MyScheduledFutureTask) {
+                // 如果任务对象是MyScheduledFutureTask类型，而不仅仅是RunnableScheduledFuture
+                // 则设置index属性便于加速查找
+                ((MyScheduledFutureTask<?>) f).heapIndex = index;
             }
         }
-    }
 
-    /**
-     * Returns true if can run a task given current run state
-     * and run-after-shutdown parameters.
-     *
-     * @param periodic true if this task periodic, false if delayed
-     */
-    boolean canRunInCurrentRunState(boolean periodic) {
-        if(periodic){
-            // 周期性任务，由continueExistingPeriodicTasksAfterShutdown决定
-            return super.isRunningOrShutdown(continueExistingPeriodicTasksAfterShutdown);
-        }else{
-            // 非周期性任务（普通延迟任务），由executeExistingDelayedTasksAfterShutdown决定
-            return super.isRunningOrShutdown(executeExistingDelayedTasksAfterShutdown);
+        /**
+         * 第k位的元素在二叉堆中上滤（小顶堆：最小的元素在堆顶）
+         *
+         * 当新元素插入完全二叉堆时，我们直接将其插入向量末尾(堆底最右侧)，此时新元素的优先级可能会大于其双亲元素甚至祖先元素，破坏了堆序性，
+         * 因此我们需要对插入的新元素进行一次上滤操作，使完全二叉堆恢复堆序性。
+         * 由于堆序性只和双亲和孩子节点相关，因此堆中新插入元素的非祖先元素的堆序性不会受到影响，上滤只是一个局部性的行为。
+         * */
+        private void siftUp(int k, RunnableScheduledFuture<?> key) {
+            while (k > 0) {
+                // 获得第k个节点逻辑上的双亲节点
+                //     0
+                //   1   2
+                //  3 4 5 6
+                // （下标减1再除2，比如下标为5和6的元素逻辑上的parent就是下标为2的元素）
+                int parent = (k - 1) >>> 1;
+
+                // 拿到双亲节点对应的元素
+                RunnableScheduledFuture<?> e = queue[parent];
+                if (key.compareTo(e) >= 0) {
+                    // 如果当前需要上滤的元素key，其值大于或等于双亲节点就停止上滤过程（小顶堆）
+                    break;
+                }
+
+                // 当前上滤的元素key，其值小于双亲节点
+                // 将双亲节点换下来，到第k位上（把自己之前的位置空出来）
+                queue[k] = e;
+                // 设置被换下来的双亲节点的index值
+                setIndex(e, k);
+
+                // 令k下标变为更小的parent，继续尝试下一轮上滤操作
+                k = parent;
+            }
+
+            // 上滤判断结束后，最后空出来的parent的下标值对应的位置由存放上滤的元素key
+            queue[k] = key;
+            // 设置key节点的index值
+            setIndex(key, k);
         }
-    }
 
-    @Override
-    public ScheduledFuture<?> schedule(Runnable command, long delay, TimeUnit unit) {
-        return null;
-    }
+        /**
+         * 第k位的元素在二叉堆中下滤（小顶堆：最小的元素在堆顶）
+         *
+         * 当优先级队列中极值元素出队时，需要在满足堆序性的前提下，选出新的极值元素。
+         * 我们简单的将当前向量末尾的元素放在堆顶，堆序性很有可能被破坏了。此时，我们需要对当前的堆顶元素进行一次下滤操作，使得整个完全二叉堆恢复堆序性。
+         * */
+        private void siftDown(int k, RunnableScheduledFuture<?> key) {
+            // half为size的一半
+            int half = size >>> 1;
+            // k小于half才需要下滤，大于half说明第k位元素已经是叶子节点了，不需要继续下滤了
+            while (k < half) {
+                // 获得第k位元素逻辑上的左孩子节点的下标
+                int child = (k << 1) + 1;
+                // 获得左孩子的元素
+                RunnableScheduledFuture<?> c = queue[child];
+                // 获得第k位元素逻辑上的右孩子节点的下标
+                int right = child + 1;
 
-    @Override
-    public ScheduledFuture<?> scheduleAtFixedRate(Runnable command, long initialDelay, long period, TimeUnit unit) {
-        return null;
-    }
+                // right没有越界，则比较左右孩子值的大小
+                if (right < size && c.compareTo(queue[right]) > 0) {
+                    // 左孩子大于右孩子，所以用右孩子和key比较，c=右孩子节点
+                    // （if条件不满足，则用左孩子和key比较，c=左孩子节点）
+                    c = queue[child = right];
+                }
 
-    @Override
-    public ScheduledFuture<?> scheduleWithFixedDelay(Runnable command, long initialDelay, long delay, TimeUnit unit) {
-        return null;
+                // key和c比较，如果key比左右孩子都小，则结束下滤
+                if (key.compareTo(c) <= 0) {
+                    break;
+                }
+
+                // key大于左右孩子中更小的那个，则第k位换成更小的那个孩子（保证上层的节点永远小于其左右孩子，保证堆序性）
+                queue[k] = c;
+                // 设置被换到上层去的孩子节点的index的值
+                setIndex(c, k);
+                // 令下标k变大为child，在循环中尝试更下一层的下滤操作
+                k = child;
+            }
+
+            // 结束了下滤操作，最后将元素key放到最后被空出来的孩子节点原来的位置
+            queue[k] = key;
+            // 设置key的index值
+            setIndex(key, k);
+        }
+
+        // ============================= 实现接口定义的方法 ======================================
+        @Override
+        public Iterator<Runnable> iterator() {
+            return null;
+        }
+
+        @Override
+        public int size() {
+            return 0;
+        }
+
+        @Override
+        public void put(Runnable runnable) throws InterruptedException {
+
+        }
+
+        @Override
+        public boolean offer(Runnable runnable, long timeout, TimeUnit unit) throws InterruptedException {
+            return false;
+        }
+
+        @Override
+        public Runnable take() throws InterruptedException {
+            return null;
+        }
+
+        @Override
+        public Runnable poll(long timeout, TimeUnit unit) throws InterruptedException {
+            return null;
+        }
+
+        @Override
+        public int remainingCapacity() {
+            return 0;
+        }
+
+        @Override
+        public int drainTo(Collection<? super Runnable> c) {
+            return 0;
+        }
+
+        @Override
+        public int drainTo(Collection<? super Runnable> c, int maxElements) {
+            return 0;
+        }
+
+        @Override
+        public boolean offer(Runnable runnable) {
+            return false;
+        }
+
+        @Override
+        public Runnable poll() {
+            return null;
+        }
+
+        @Override
+        public Runnable peek() {
+            return null;
+        }
     }
 }
